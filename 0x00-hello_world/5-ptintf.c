aa4c17ac6b915c248358f7fdaf2b9a632a7fbead
BenzYMa26 <benzyma26@gmail.com>
2023-09-08 03:07:18 -0700

    printed with printf

---
+++ 0x00-hello_world/5-ptintf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited properly or non zero if otherwise
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
