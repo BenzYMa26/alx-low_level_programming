7575ffba8606d1b4efc861152ad77d5b9747e9dd
BenzYMa26 <benzyma26@gmail.com>
2023-09-13 05:01:01 -0700

    times table code with _putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,6 +21,13 @@ _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(k / 10) + '0');
+-putchar((k % 10) + '0');
+}
 }
 _putchar('n');
 }
