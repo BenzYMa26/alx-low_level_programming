e46e27cfd289c5d13058104f171020adc5c1799e
BenzYMa26 <benzyma26@gmail.com>
2023-09-14 06:03:23 -0700

    code to check uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_isupper - checks for uppercase character
+  * @c: the character to be checked
+  * Return: 1 if c is uppercase or 0 if otherwise
+  */
+int_isupper(int c)
+{
+if (c >= "A" && c <= "Z")
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this dir. contains answers to more nexted loop function tasks
