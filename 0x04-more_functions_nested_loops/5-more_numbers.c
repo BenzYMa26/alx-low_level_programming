79ef89fefa71997ff733f950414357a04f1287bf
BenzYMa26 <benzyma26@gmail.com>
2023-09-15 05:16:10 -0700

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,12 @@ void more_numbers(void)
 int i;
 int a;
 
-for (a = 0; a <= 10; a++)
+for (a = 0; a < 10; a++)
 {
 	for (i = 0; i < 15; i++)
 	{
 		_putchar(i + '0');
 	}
+	_putchar('\n');
 }
-_putchar('\n');
 }
