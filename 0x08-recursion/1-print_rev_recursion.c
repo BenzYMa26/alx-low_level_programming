cf4d0f327cc7d35391d79a7d577f18fbeb4e0bab
BenZYma26 <ebenezamadu@gmail.com>
2023-10-25 14:31:34 -0700

    done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,16 +4,13 @@
 /**
  * _print_rev_recursion - this function will print strings in reverse
  * @s: the string to be printed
- * Return: new line
  */
 void _print_rev_recursion(char *s)
 {
-
 	if (*s)
 	{
 		_print_rev_recursion(++s);
 		_putchar(*s);
 	}
-	_putchar('\n');
 }
 
