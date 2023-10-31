edeb5bc4d069edd52e0ff868c3e212f13f157fcf
BenZYma26 <ebenezamadu@gmail.com>
2023-10-31 08:30:20 -0700

    done

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @b: memory area to be filled
+ * @d: char to copy
+ * @c: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *b, char d, unsigned int c)
+{
+unsigned int i;
+unsigned int n;
+
+for (i = 0; i < n; i++)
+{
+b[i] = d;
+}
+
+return (b);
+}
