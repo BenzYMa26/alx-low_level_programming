7142a9e8e94ca2d5842dec61356c8093696b84a6
BenZYma26 <ebenezamadu@gmail.com>
2023-11-01 13:23:16 -0700

    doen

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints the number of arguments
+ * passed to it, followed by a new line
+ * @argx: number of varaiables
+ * @argv: array of argument
+ * Return: returns 0
+ */
+
+int main(int argx, char *argv[]__attribute__((unused)))
+{
+	printf("%d \n", argx);
+	return (0);
+}
